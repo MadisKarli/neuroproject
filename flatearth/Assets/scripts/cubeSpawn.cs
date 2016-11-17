0813f0759a310915ee4b2a5c1a2a2f477bc13e5c
MadisKarli <matukoppel@gmail.com>
2016-11-17 13:54:48 +0200

    corrected physics and stuff

---
+++ flatearth/Assets/scripts/cubeSpawn.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cubeSpawn : MonoBehaviour {
+    [SerializeField] public GameObject cube;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(cube, this.transform);
+        }
+	}
+}
